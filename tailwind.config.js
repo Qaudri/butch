ea54ce26825269aa07df109d02dc52fba0ab24e5
Muhammad Abdul Quadir <aquadirmuhammad@gmail.com>
2024-05-08 15:57:31 +0000

    init

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#780584',
+        'secondary': '#c02827'
+      }
+    },
+  },
+  plugins: [],
+}
+
